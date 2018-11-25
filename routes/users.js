4f992f3afb0040fe1d3e9c00f4c3f535ea65cc1e
Charles Quelos <charles.quelos@gmail.com>
2018-11-25 15:38:01 +0100

    WS mock sur /users/mock

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource, petite bite');
 });
 
+router.get('/mock', function(req, res, next) {
+  res.send({foo: 'bar'});
+});
+
 module.exports = router;
